72e6b2d7b3a2cfd6d9a5ff71021721d66ccee7b5
Emma Saccone <emma@saccone.org>
2025-04-20 21:53:52 -0400

    implement project list table view
    Added static list of sample crochet projects and connected table view to ProjectListViewController. Fixed storyboard links and initial view controller setup.

--- crochetTracker/ProjectListViewController.swift
+++ crochetTracker/ProjectListViewController.swift
@@ -4,17 +4,44 @@
 //
 //  Created by Emma Saccone on 4/20/25.
 //
-
 import UIKit
 
-class ProjectListViewController: UIViewController {
+class ProjectListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    // Connected to the Table View in Main.storyboard
+    @IBOutlet weak var tableView: UITableView!
+    
+    //array of test projects and rownumbers
+    var projects: [CrochetProject] = [
+        CrochetProject(name: "Scarf", currentRow: 5),
+        CrochetProject(name: "Blanket", currentRow: 12),
+        CrochetProject(name: "Hat", currentRow: 2)
+    ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Set the table view's data source and delegate to this view controller
+                tableView.dataSource = self
+                tableView.delegate = self
     }
     
+    // Tells the table how many rows to show
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+
+    // Creates the content for each row (aka cell)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectCell", for: indexPath)
+        let project = projects[indexPath.row]
+        cell.textLabel?.text = "\(project.name) — Row \(project.currentRow)"
+        return cell
+    }
 
     /*
     // MARK: - Navigation
@@ -27,3 +54,4 @@ class ProjectListViewController: UIViewController {
     */
 
 }
+
